ddb3740dbf566c52661e718bf745fc993e6a152f
RogoJuerg <juergen.rogozin@gmail.com>
2022-11-24 19:18:03 +0100

    added multiple classes

--- js/game.js
+++ js/game.js
@@ -1,14 +1,11 @@
 
 let canvas;
-let ctx;
-let character = new Image();
+let world;
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
 
+    console.log('My Character is ', world.character);
 
-    character.src = '../img/2_character_pepe/2_walk/W-21.png';
-
-    ctx.drawImage(character, 20, 20, 50, 150);
 }
