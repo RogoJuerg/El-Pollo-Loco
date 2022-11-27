ca5c07103a51c36b6ea1a0eb9ba7724ec1ef005b
RogoJuerg <juergen.rogozin@gmail.com>
2022-11-27 02:17:06 +0100

    pushing changes

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -5,9 +5,23 @@ class MovableObject {
     height = 150;
     width = 100;
 
+    imageCache = {};
+
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
+     */
+
+    loadImages(arr) {
+        arr.forEach(() => {
+            let image = new Image();
+            image.src = arr;
+            this.imageCache[path] = path;
+        });
     }
 
     moveRight() {
