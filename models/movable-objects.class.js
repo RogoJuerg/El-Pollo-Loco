9e8df5bc5e021b834383e87ee9998f11919f90f3
RogoJuerg <juergen.rogozin@gmail.com>
2022-11-27 19:54:25 +0100

    added animations

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -6,21 +6,23 @@ class MovableObject {
     width = 100;
 
     imageCache = {};
+    currentImage = 0;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
+
     /**
      * 
      * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
      */
 
     loadImages(arr) {
-        arr.forEach(() => {
-            let image = new Image();
-            image.src = arr;
-            this.imageCache[path] = path;
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
         });
     }
 
